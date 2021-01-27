d9a9185607b1a5374691afe6af9fcf48126cf65a
sylwiagutowska <syl.gutowska@gmail.com>
2021-01-27 18:01:26 +0100

    it is finished, but dunno if works

--- hopfield_full.py
+++ hopfield_full.py
@@ -6,7 +6,8 @@ import hopfield_tetra
 from scipy.special import sph_harm
 from scipy.integrate import quad
 from scipy.misc import derivative
-
+sqrtpm1=(np.pi)**(-0.5)
+degauss=0.02
 #############FOR Nb eta=7.627eV/ang^2 [papaconsta.. 2002] or 4.7 eV/ang^2 (allen-dynes)
 
 def sil(n):
@@ -19,7 +20,7 @@ def sil(n):
   return float(a)
 
 def clebsch_gordon(l1,m1,l2,m2,l3,m3):
-# if m3!=(m1+m2): return 0
+# if m3!=(m2+m1): return 0
  if (l3+l1-l2)<0 or (l3-l1+l2)<0 or (l1+l2-l3)<0: return 0 
  fac1=( ( (2*l3+1.)*sil(l3+l1-l2)*sil(l3-l1+l2)*sil(l1+l2-l3) )/ sil(l1+l2+l3+1) )**0.5
  fac2=( sil(l3+m3)*sil(l3-m3)*sil(l1-m1)*sil(l1+m1)*sil(l2-m2)*sil(l2+m2) )**0.5
@@ -30,8 +31,8 @@ def clebsch_gordon(l1,m1,l2,m2,l3,m3):
  return fac1*fac2*fac3
 
 def three_y(l1,m1,l2,m2,l3,m3):
- return ( (2*l1+1.)*(2*l2+1.)/(4.*np.pi*(2*l3+1.)) )**0.5\
-        *clebsch_gordon(l1,0,l2,0,l3,0)*clebsch_gordon(l1,m1,l2,m2,l3,m3)
+ return round(( (2*l1+1.)*(2*l2+1.)/(4.*np.pi*(2*l3+1.)) )**0.5\
+        *clebsch_gordon(l1,0,l2,0,l3,0)*clebsch_gordon(l1,m1,l2,m2,l3,m3),9)
 
 def ynre(m,l,tet,fi):
  return np.real(sph_harm(m1,l1,fi,tet))
@@ -39,43 +40,21 @@ def ynim(m,l,tet,fi):
  return np.imag(sph_harm(m1,l1,fi,tet))
 
 def spec_int(l1,m1,l2,m2,l3,m3):
-# if m1==l1: return 0.
-# j=complex(0,1)
- #sph_harm(m,l,fi,teta)
+# if m1==0: return 0.
  out1=quad(lambda tet: np.real(sph_harm(m1,l1,0,tet)*np.conj(sph_harm(m2,l2,0,tet))*sph_harm(m3,l3,0,tet)), 0.0, np.pi)
  out2=quad(lambda tet: np.imag(sph_harm(m1,l1,0,tet)*np.conj(sph_harm(m2,l2,0,tet))*sph_harm(m3,l3,0,tet)), 0.0, np.pi)
  return 2*np.pi*m1*complex(-round(out2[0],6),round(out1[0],6)) #calka*2*pi*m*i
 
 
 ################RADWF AND V are already multiplied by r
-
 '''
-from sympy import Ynm, Symbol,pi,symbols
-from sympy import integrate,diff,conjugate, I,Piecewise,Ne,sin
-n = symbols('n', integer = True)
-m = symbols('m', integer = True)
-o = symbols('o', integer = True)
-p = symbols('p', integer = True)
-r = symbols('r', integer = True)
-s = symbols('s', integer = True)
-theta = Symbol("theta")
-phi = Symbol("phi")
-#print spec_int(1,-1,0,0,1,1)
-print Ynm(n, m, theta, phi)
-#print integrate(diff(Ynm(n,m,theta,phi),phi)*Ynm(o,p,theta,phi).conjugate()*Ynm(r,s,theta,phi),(phi,0,2*pi))
-print integrate(Ynm(n,m,theta,0)*Ynm(o,p,theta,0).conjugate()*Ynm(r,s,theta,0),(theta,0,pi))
-#integrate(sin(theta)*Ynm(n, m, theta, phi)*Ynm(o, p, theta, phi).conjugate()*Ynm(r, s, theta, phi), (theta))
-#diff(Ynm(n,m,theta,phi),phi)*
-#print (integrate(Ynm(1, 0, theta, phi)*Ynm(1, 1, theta, phi)*Ynm(2, 1, theta, phi)/2
-#,theta))
-
 for l1 in range(2):
- for m1 in range(-l1,l1+1): 
+ for m1 in range(-l1,l1+1):
   for l2 in range(2):
    for m2 in range(-l2,l2+1):
-    for l3 in range(2):
+    for l3 in range(3):
      for m3 in range(-l3,l3+1):
-      if m3==m2-m1: print(l1,m1,l2,m2,l3,m3,spec_int(l1,m1,l2,m2,l3,m3))
+      print l1,m1,l2,m2,l3,m3,three_y(l1,m1,l3,m3,l2,m2),three_y(l1,m1,l2,m2,l3,m3)
 exit()
 '''
 
@@ -146,15 +125,19 @@ h=open('RADWF/RADWF.radwf','r')
 tmp=h.readlines()
 h.close()
 
-RADWF=[]   #RADWF[h][i][j][k][0-3] h-kpoint, i-atoms, j- l (orbital No), k - r-mesh, [0-1]-large or small component of radwf, [2-3]- large or small component of udot
+RADWF=[]   #RADWF[i][j][k][0-3] i-atoms, j- l (orbital No), k - r-mesh, [0-1]-large or small component of radwf, [2-3]- large or small component of udot
+RADWFsmall=[]
 mesh_info=[]
 for i in tmp:
  if str(nr) in i.split(): 
   RADWF.append([])
+  RADWFsmall.append([])
   mesh_info.append([float(m) for m in i.split()])
  elif len(i.split())==1: RADWF[-1].append([])
  else:
-  try: RADWF[-1][-1].append([float(i.split()[0]),float(i.split()[1]),float(i.split()[2]),float(i.split()[3])])
+  try: 
+   RADWF[-1][-1].append(np.array([float(i.split()[0]),float(i.split()[2])]))
+   RADWFsmall[-1][-1].append(np.array([float(i.split()[1]),float(i.split()[3])]))
   except:continue
 n_l=min([ len(i) for i in RADWF])
 
@@ -202,6 +185,23 @@ for i in range(len(RADWF)):
   h.write('\n')
  h.close() 
 
+print('Read EF and total DOS...'),
+
+h=open('DOS/tot/DOS.outputt','r')
+tmp=h.readlines()
+h.close()
+sign=0
+for j in tmp[-10:]:
+ if sign==1:
+   TOTDOS=[ round(float(m),5) for m in j.split()]
+   break
+ if ' ******** EF and DOS at fermi level *******' in j:
+   sign=1
+for j in TOTDOS:
+  print(str(j)+' '),
+print(' ')
+
+E_f=TOTDOS[0]
 
 print('Reading band energies...'),
 ENE=[]
@@ -225,7 +225,7 @@ if i==1:
 #ENE[i][j] i-kpoint, j- band
 for i in tmp:
   if len(i)>4 and int(i[-4])==len(ENE)+1: 
-   kweights.append(i[-1])
+   kweights.append(float(i[-1]))
    ENE.append([])
   elif len(i)==2: 
    ENE[-1].append(i[1])
@@ -233,8 +233,34 @@ for i in tmp:
 #et[ibnd][tetra[i][nt]]
 print("; No of noneq kpoints="+str(len(ENE))), 
 n_k,n_band=len(kweights),min([ len(i) for i in ENE])
-ENE=[ [float(ENE[j][i]) for j in range(len(ENE))] for i in range(n_band)]
-print("; No of bands="+str(len(ENE)))
+ENE=[ [float(ENE[j][i]) for i in range(n_band)] for j in range(len(ENE))]
+#ENE=[ [float(ENE[j][i]) for j in range(len(ENE))] for i in range(n_band)]
+print("; No of bands="+str(len(ENE[0])))
+ENE_weights=[ [0 for i in range(n_band)] for j in range(n_k)]
+# weights for integrals over k (cold smearing)
+for k in range(n_k):
+ for iband in range(n_band):
+          eef=E_f-ENE[k][iband]
+          arg=min(200., (eef - 2.**(-0.5) ) **2)
+          ENE_weights[k][iband]=(kweights[k]*sqrtpm1*np.exp(-arg)*(2.-(2**0.5*eef)))
+'''
+ENE2=[ [float(ENE[j][i]) for j in range(len(ENE)) ] for i in range(n_band)]
+
+print('Which bands cross Fermi level: '),
+band_f=[]
+for ni,i in enumerate(ENE2):
+ old=i[0]
+ is_fermi=0
+ for j in i:
+  if (j-E_f)*(old-E_f)<0:
+   is_fermi=1
+   band_f.append(ni)
+   break
+
+n_band=len(band_f)
+print n_band,'bands:',band_f
+ENE=[ [float(ENE[j][i]) for i in band_f] for j in range(len(ENE))]
+'''
 
 
 print('Reading alm coefficients and kpoints...'),
@@ -251,7 +277,7 @@ if len(tmp)==0:
   tmp.extend([m.split()  for m in h.readlines() if len(m.split())!=0])
   h.close()
 
-#ALMBLM[k][i][j][l][m][0-3] k-kpoint, i-atoms, j-band, l (orbital No), m, [0: Re[Alm], 1: Im[Alm], 2: Re[Blm], 3: Im[Blm]]
+#ALMBLM[k][i][j][l][m][0-3] k-kpoint, i-atoms, j-band, l (orbital No), m, [0: Re[Alm]+j*Im[Alm], 1:  Re[Blm]+j*Im[Blm]]
 ALMBLM=[] 
 #NONEQ=[] #list of nonequiv kpoints
 for i in tmp:
@@ -262,7 +288,9 @@ for i in tmp:
  elif 'weight' in i[-1]:  
   ALMBLM[-1][-1].append( [ [] for k in range(n_l) ] )
  elif len(i)>10:
-  ALMBLM[-1][-1][-1][int(i[0])].append([float(i[3]),float(i[4]),float(i[6]),float(i[7])]) 
+  ALMBLM[-1][-1][-1][int(i[0])].append([complex(float(i[3]),float(i[4])),complex(float(i[6]),float(i[7]))]) 
+
+
 
 '''
 h=open('kpoints.dat','w')
@@ -281,21 +309,6 @@ h.close()
 print(" No of noneq kpoints="+str(n_k))
  
 
-print('Read EF and total DOS...'),
-
-h=open('DOS/tot/DOS.outputt','r')
-tmp=h.readlines()
-h.close()
-sign=0
-for j in tmp[-10:]:
- if sign==1:
-   TOTDOS=[ round(float(m),5) for m in j.split()]
-   break
- if ' ******** EF and DOS at fermi level *******' in j:
-   sign=1
-for j in TOTDOS:
-  print(str(j)+' '),
-print(' ')
 
 
 
@@ -321,7 +334,7 @@ print len(YLM[3]), len(YLM[3][0]), (YLM[3][0][0])
 #Vtot=[] #[atom][lm][r]
 #LM=[] #[atom][list of lm 
 n_l=5
-
+Hopfield,Hopfield2=0.,0.
 
 dtet=pi/n_tet
 dfi=2*pi/n_fi
@@ -329,59 +342,91 @@ sintet=np.sin(np.linspace(0,pi,n_tet))
 dr=[[0]+[ RMESH[at][i]-RMESH[at][i-1] for i in range(1,nr)] for at in range(n_at)]
 print len(dr[0])
 print('Calculate eta...')
+k,kp,iband,jband=0,0,0,0
 for at in range(1):#n_at):
- for nlm1 in range(len(LM[at])):
+ do_b=0
+ for nlm1 in range(1,len(LM[at])):
   print nlm1
   [l1,m1]=LM[at][nlm1]
-  for nlm2 in range(len(LM[at])):
+  if m1==0: 
+   Bx=0.
+   do_b=0
+  else: do_b=1
+  for nlm2 in range(1,len(LM[at])):
    [l2,m2]=LM[at][nlm2]
-   for l3 in range(10):#len(LM[at])):
+   if m2==0: 
+    Bx=0.
+    do_b=0
+   else: do_b=1
+   for l3 in range(6):#len(LM[at])):
     for m3 in range(-l3,l3+1):#len(LM[at])):
-     for l4 in range(10):#len(LM[at])):
+     for l4 in range(6):#len(LM[at])):
+      print l1,l2,l3,l4,Hopfield,Hopfield2
       for m4 in range(-l4,l4+1):#len(LM[at])):
-       Ax1=three_y(l1,m1,l4,m4,l3,m3)
-       if m4==m3-m1 and m1!=0:  Bx1=spec_int(l1,m1,l3,m3,l4,m4)
-       else: Bx1=0.
-       for l5 in range(10):#len(LM[at])):
+
+       if m4==m3-m1:  
+        Ax1=three_y(l1,m1,l4,m4,l3,m3)
+        if do_b: Bx1=spec_int(l1,m1,l3,m3,l4,m4)
+       else: 
+        continue
+
+       B_and_C_r,A_r=np.array([0.j,0.j]),np.array([0.j,0.j])
+       B_and_C_r2,A_r2=np.array([0.j,0.j]),np.array([0.j,0.j])
+       for nr1,r1 in enumerate(RMESH[at]):   
+        dv1dr=Vtot[at][nlm1][nr1]-Vtot[at][nlm1][nr1-1]
+        dr1=dr[at][nr1]
+        B_and_C_r+=r1*Vtot[at][nlm1][nr1]*np.conjugate(RADWF[at][l3][nr1])*RADWF[at][l4][nr1]*dr1
+        A_r+=r1**2. * dv1dr *np.conjugate(RADWF[at][l3][nr1])*RADWF[at][l4][nr1]
+        B_and_C_r2+=r1*Vtot[at][nlm1][nr1]*np.conjugate(RADWF[at][l3][nr1])*RADWF[at][l4][nr1][::-1]*dr1
+        A_r2+=r1**2. * dv1dr *np.conjugate(RADWF[at][l3][nr1])*RADWF[at][l4][nr1][::-1]      
+       #wsp=np.conjugate(ALMBLM[k][at][iband][l3][m3])*ALMBLM[kp][at][jband][l4][m4]
+       #B_and_C_r *=wsp
+       #A_r *=wsp
+
+       for l5 in range(6):#len(LM[at])):
         for m5 in range(-l5,l5+1):#len(LM[at])):
-         for l6 in range(10):#len(LM[at])):
+
+         I_kp,I2_kp=np.array([0.j,0.j]),np.array([0.j,0.j])
+         for kp in range(len(ALMBLM)):
+            for iband in range(len(ALMBLM[kp])):
+#             for jband in range(n_band):
+              I_kp+=ENE_weights[kp][iband]*np.conjugate(ALMBLM[kp][at][iband][l5][m5+l5])*ALMBLM[kp][at][iband][l4][m4+l4]
+              I2_kp+=ENE_weights[kp][iband]*np.conjugate(ALMBLM[kp][at][iband][l5][m5+l5])*ALMBLM[kp][at][iband][l4][m4+l4][::-1]  
+
+
+         for l6 in range(6):#len(LM[at])):
           for m6 in range(-l6,l6+1):#len(LM[at])):
-           Ax=Ax1*three_y(l2,m2,l6,m6,l5,m5)
-           if m6==m5-m2 and m4==m3-m1 and m2!=0:  Bx=Bx1*spec_int(l2,m2,l5,m5,l6,m6)
-           else: Bx=0.
-           Cx=-m1*m2*Ax 
 
-           '''
-           for nr1,r1 in enumerate(RMESH[at]):   
-            if nr1==0: continue  
-            if nr1==2: break
-            dv1dr=Vtot[at][nlm1][nr1]-Vtot[at][nlm1][nr1-1]
+           if m6==m5-m2:  
+            Ax=Ax1*three_y(l2,m2,l6,m6,l5,m5)
+	    if do_b: Bx=Bx1*spec_int(l2,m2,l5,m5,l6,m6)
+            Cx=-m1*m2*Ax 
+           else: 
+             continue
+
+	   B_and_C_rp,A_rp=np.array([0.j,0.j]),np.array([0.j,0.j])
+	   B_and_C_rp2,A_rp2=np.array([0.j,0.j]),np.array([0.j,0.j])
+           for nr1,r1 in enumerate(RMESH[at]):
+            dv1dr=Vtot[at][nlm2][nr1]-Vtot[at][nlm2][nr1-1]
             dr1=dr[at][nr1]
-            for nr2,r2 in enumerate(RMESH[at]):
-             if nr2==0: continue
-             if nr2==50: break
-            '''
-      print ' ',l1,m1,' ',l2,m2,' ',l3,l4,Ax,Bx,Cx
-'''
-     dr2=dr[at][nr2]
-     dv2dr=Vtot[at][nlm2][nr2]-Vtot[at][nlm2][nr2-1]
-     dvdr=dv1dr*dv2dr
-     for fi1 in range(1,n_fi):
-      for tet1 in range(1,n_tet):
-        dy1dfi=YLM[at][nlm1][fi1][tet1]-YLM[at][nlm1][fi1-1][tet1]
-        dy1dtet=YLM[at][nlm1][fi1][tet1]-YLM[at][nlm1][fi1][tet1-1]
-        for fi2 in range(1,n_fi):
-         for tet2 in range(1,n_tet):
-          dy2dfi=YLM[at][nlm2][fi2][tet2]-YLM[at][nlm2][fi2-1][tet2]
-          dy2dtet=YLM[at][nlm1][fi2][tet2]-YLM[at][nlm1][fi2][tet2-1]
-          dvr=r1*r1*r2*r2*dfi*dtet*sintet[tet1]*dfi*dtet*sintet[tet2]*dvdr\
-              *YLM[at][nlm1][fi1][tet1]*YLM[at][nlm2][fi2][tet2]
-          dvfi=dr1*dr2*r1*r2*dtet*dtet\
-                *Vtot[at][nlm1][nr1]*dy1dfi*Vtot[at][nlm2][nr2]*dy2dfi
-          dvtet=dr1*dr2*r1*r2*dfi*dfi*sintet[tet1]*sintet[tet2]\
-                *Vtot[at][nlm1][nr1]*dy1dtet*Vtot[at][nlm2][nr2]*dy2dtet
-'''
+            B_and_C_rp+=r1*Vtot[at][nlm2][nr1]*np.conjugate(RADWF[at][l5][nr1])*RADWF[at][l6][nr1]*dr1
+            A_rp+=r1**2. * dv1dr *np.conjugate(RADWF[at][l5][nr1])*RADWF[at][l6][nr1]
+            B_and_C_rp2+=r1*Vtot[at][nlm2][nr1]*np.conjugate(RADWF[at][l5][nr1])*RADWF[at][l6][nr1][::-1]*dr1
+            A_rp2+=r1**2. * dv1dr *np.conjugate(RADWF[at][l5][nr1])*RADWF[at][l6][nr1][::-1]
+	    
+	   I_k,I2_k=np.array([0.j,0.j]),np.array([0.j,0.j])
+           for k in range(len(ALMBLM)):
+            for iband in range(len(ALMBLM[k])):
+#             for jband in range(n_band):
+              I_k +=ENE_weights[k][iband]*np.conjugate(ALMBLM[k][at][iband][l3][m3+l3])*ALMBLM[k][at][iband][l6][m6+l6]
+              I2_k+=ENE_weights[k][iband]*np.conjugate(ALMBLM[k][at][iband][l3][m3+l3])*ALMBLM[k][at][iband][l6][m6+l6][::-1]
+
+           Hopfield+=np.dot(B_and_C_r*B_and_C_rp*(Bx+Cx)+A_r*A_rp*Ax, I_k*I_kp)
+           Hopfield2+=np.dot(B_and_C_r2*B_and_C_rp2*(Bx+Cx)+A_r2*A_rp2*Ax, I2_k*I2_kp)
 
 
+#ENE[i][j] i-kpoint, j- band
+#RADWF[h][i][j][k][0-1] h-kpoint, i-atoms, j- l (orbital No), k - r-mesh, [0]-radwf, [1]-udot
+#ALMBLM[k][i][j][l][m][0-3] k-kpoint, i-atoms, j-band, l (orbital No), m, [0: Re[Alm]+j*Im[Alm], 1:  Re[Blm]+j*Im[Blm]]
 
 
